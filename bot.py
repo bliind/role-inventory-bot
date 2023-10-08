21366e058980c1f4c3956cff823854083bc55e6d
Mike Sweeney <blindx@gmail.com>
2023-10-08 11:42:31 -0400

    remove unneeded intent

--- bot.py
+++ bot.py
@@ -18,7 +18,6 @@ load_config()
 class MyClient(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
-        intents.message_content = True
         super().__init__(command_prefix='>', intents=intents)
         self.synced = False
 
