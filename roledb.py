3e93a88446062055e8829fb689c5cff4a62f3aa4
Mike Sweeney <blindx@gmail.com>
2024-07-16 22:45:56 -0400

    Set up special XP embed to remove SR roles

--- roledb.py
+++ roledb.py
@@ -28,6 +28,7 @@ async def check_user(user_id: int):
         async with aiosqlite.connect('db.db') as db:
             cursor = await db.execute('SELECT * FROM inventory WHERE user_id = ?', (user_id,))
             row = await cursor.fetchone()
+            await cursor.close()
         if row:
             return True
         else:
@@ -42,6 +43,7 @@ async def get_roles(user_id: int):
         async with aiosqlite.connect('db.db') as db:
             cursor = await db.execute('SELECT roles FROM inventory WHERE user_id = ?', (user_id,))
             row = await cursor.fetchone()
+            await cursor.close()
     except Exception as e:
         print('Failed to get user roles:')
         print(e, user_id)
@@ -50,3 +52,62 @@ async def get_roles(user_id: int):
         return row[0].split(',')
     except Exception as e:
         return []
+
+async def add_sr_role(role_name: str, role_id: int):
+    try:
+        async with aiosqlite.connect('db.db') as db:
+            await db.execute('INSERT INTO sr_role (role_name, role_id) VALUES (?, ?)', (role_name, role_id))
+            await db.commit()
+        return True
+    except Exception as e:
+        print('Failed to add SR role:')
+        print(e, role_name, role_id)
+        return False
+
+async def remove_sr_role(role_id: int):
+    try:
+        async with aiosqlite.connect('db.db') as db:
+            await db.execute('DELETE FROM sr_role WHERE role_id = ?', (role_id,))
+            await db.commit()
+        return True
+    except Exception as e:
+        print('Failed to remove SR role:')
+        print(e, role_id)
+        return False
+
+async def list_sr_roles():
+    try:
+        async with aiosqlite.connect('db.db') as db:
+            db.row_factory = aiosqlite.Row
+            cursor = await db.execute('SELECT * FROM sr_role')
+            rows = await cursor.fetchall()
+            await cursor.close()
+            return rows
+    except Exception as e:
+        print('Failed to get SR roles:')
+        print(e)
+        return False
+
+async def set_no_xp_role(role_id: int):
+    try:
+        async with aiosqlite.connect('db.db') as db:
+            await db.execute('UPDATE no_xp_role SET role_id = ?', (role_id,))
+            await db.commit()
+        return True
+    except Exception as e:
+        print('Failed to set No XP role:')
+        print(e, role_id)
+        return False
+
+async def get_no_xp_role():
+    try:
+        async with aiosqlite.connect('db.db') as db:
+            db.row_factory = aiosqlite.Row
+            cursor = await db.execute('SELECT * FROM no_xp_role')
+            row = await cursor.fetchone()
+            await cursor.close()
+        return row['role_id']
+    except Exception as e:
+        print('Failed to get SR roles:')
+        print(e)
+        return False
