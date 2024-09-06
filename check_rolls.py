feaee9f3905e06c49aa5686e627af7bcf38ba378
Mike Sweeney <blindx@gmail.com>
2024-09-06 18:09:28 -0400

    Give myself more utility commands

--- check_rolls.py
+++ check_rolls.py
@@ -9,7 +9,7 @@ def humantime(timestamp):
     date = datetime.datetime.fromtimestamp(timestamp)
     return date.strftime('%Y-%m-%d %H:%M:%S')
 
-async def get_users_roles(user_id: int):
+async def get_user_spins(user_id: int):
     try:
         sql = 'SELECT * FROM gamba WHERE user_id = ? ORDER BY datestamp ASC'
 
@@ -32,11 +32,29 @@ async def get_users_roles(user_id: int):
         print(e)
         return False
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        raise SystemError('error: provide user id')
+async def get_spin_counts():
+    try:
+        sql = 'SELECT count(id) as total_spins, user_id FROM gamba GROUP BY user_id ORDER BY total_spins ASC'
 
-    rolls = asyncio.run(get_users_roles(sys.argv[1]))
+        async with aiosqlite.connect(dbfile) as db:
+            db.row_factory = aiosqlite.Row
+            cursor = await db.execute(sql)
+            rows = await cursor.fetchall()
+            await cursor.close()
+
+        out = []
+        for row in rows:
+            out.append({
+                "total_spins": row['total_spins'],
+                "user_id": row['user_id'],
+            })
+        return out
+    except Exception as e:
+        print(e)
+        return False
+
+def check_user_spins(user_id: int):
+    rolls = asyncio.run(get_user_spins(user_id))
     last = None
     for roll in rolls:
         if last:
@@ -44,3 +62,20 @@ if __name__ == '__main__':
         else:
             print(humantime(roll['datestamp']))
         last = roll['datestamp']
+
+def check_total_spins():
+    rolls = asyncio.run(get_spin_counts())
+    for roll in rolls:
+        print(f'{roll["user_id"]} - {roll["total_spins"]}')
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        raise SystemError('error: provide action')
+
+    if sys.argv[1] == 'user':
+        if len(sys.argv) < 3:
+            raise SystemError('error: provide user id')
+        check_user_spins(sys.argv[2])
+
+    elif sys.argv[1] == 'totals':
+        check_total_spins()
